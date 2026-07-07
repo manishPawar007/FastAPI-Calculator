ac16643d06169a561d66514bd05d8211f79a9d39
manishPawar007 <mspawar2008@gmail.com>
2026-07-07 15:41:59 +0530

    Fixed Render deployment

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
+from pathlib import Path
 
-app = FastAPI()
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+
+app = FastAPI(title="FastAPI Calculator")
+
+# CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -9,31 +14,46 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Get current directory
+BASE_DIR = Path(__file__).resolve().parent
+
+
+# Home Page
+@app.get("/")
+async def home():
+    return FileResponse(BASE_DIR / "index.html")
+
+
+# Addition
 @app.get("/addition/{a}/{b}")
 async def add_numbers(a: int, b: int):
-    result = a + b
-    return {"result": result}
+    return {"result": a + b}
 
+
+# Subtraction
 @app.get("/subtraction/{a}/{b}")
 async def subtract_numbers(a: int, b: int):
-    result = a - b
-    return {"result": result}
+    return {"result": a - b}
 
+
+# Multiplication
 @app.get("/multiplication/{a}/{b}")
 async def multiply_numbers(a: int, b: int):
-    result = a * b
-    return {"result": result}
+    return {"result": a * b}
 
+
+# Division
 @app.get("/division/{a}/{b}")
 async def divide_numbers(a: int, b: int):
     if b == 0:
         return {"error": "Division by zero is not allowed."}
-    result = a / b
-    return {"result": result}
+    return {"result": a / b}
 
+
+# Modulus
 @app.get("/modulus/{a}/{b}")
 async def modulus_numbers(a: int, b: int):
     if b == 0:
         return {"error": "Modulus by zero is not allowed."}
-    result = a % b
-    return {"result": result}
+    return {"result": a % b}
